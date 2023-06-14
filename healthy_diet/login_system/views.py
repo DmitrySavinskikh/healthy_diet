9fc5493c19630634494041ceec236d120531ea63
Dmitry Savinskikh <d.savinskikh@gmail.com>
2023-06-14 11:24:37 +0700

    blocked content for none authenticated users

--- healthy_diet/login_system/views.py
+++ healthy_diet/login_system/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 def goto_firstpage(request):
